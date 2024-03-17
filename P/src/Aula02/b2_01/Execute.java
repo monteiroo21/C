ab84ac627b25d5c2860add465afb338ad66e1156
Joao <joaomonteiro29@ua.pt>
2024-03-17 11:07:05 +0000

    ex01 done

--- P/src/Aula02/b2_01/Execute.java
+++ P/src/Aula02/b2_01/Execute.java
@@ -1,28 +1,33 @@
-package Aula02.b2_01;
-
-import org.antlr.v4.runtime.tree.TerminalNode;
-
 @SuppressWarnings("CheckReturnValue")
 public class Execute extends HelloBaseVisitor<String> {
 
-   @Override
-   public String visitGreeting(HelloParser.GreetingsContext ctx) {
-      System.out.print("Hello,");
-      for (TerminalNode name : ctx.ID()) {
-         System.out.print(" " + name.getText());
-      }
-      System.out.println("!");      
-      return null;
+   @Override public String visitMain(HelloParser.MainContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
    }
 
-   @Override
-   public String visitBye(HelloParser.ByeContext ctx) {
-      System.out.print("Goodbye,");
-      for (TerminalNode name : ctx.ID()) {
-         System.out.print(" " + name.getText());
-      }
-      System.out.println("!");  
-      return null;
-   }   
+   @Override public String visitGreetings(HelloParser.GreetingsContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
 
+   @Override public String visitBye(HelloParser.ByeContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitName(HelloParser.NameContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitWords(HelloParser.WordsContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
 }
--- P/src/Aula02/b2_01/Hello.g4
+++ P/src/Aula02/b2_01/Hello.g4
@@ -1,8 +1,10 @@
 grammar Hello;
+
 main : (greetings | bye)*;
 greetings : 'hello' name ;
 bye : 'bye' name ;
 name : words+;
 words : ID;
+
 ID : [a-zA-Z]+ ;
 WS : [ \t\r\n]+ -> skip ;
